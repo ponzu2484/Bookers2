eb0c338f8fef07ea58a91b398ae94d50f6058d14
EC2 Default User <ec2-user@ip-172-31-24-188.ap-northeast-1.compute.internal>
2022-08-21 05:52:51 +0000

    [Update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def index
+  end
+  
   def show
     @user = User.find(params[:id])
     @books = @user.books
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"homes#top"
   resources :books, only: [:new, :index, :show, :edit]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:index, :show, :edit]
+  get "/home/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
